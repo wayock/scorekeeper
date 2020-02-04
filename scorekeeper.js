1ad30c9718b9a1a46e47998cdf545f5f9c127788
Matthew Wayock <wayock@gmail.com>
2020-02-03 22:43:28 -0500

    scorekeeper app created

---
+++ scorekeeper.js
@@ -0,0 +1,145 @@
+let p1Button = document.getElementById("p1");
+let p2Button = document.querySelector("#p2");
+let p3Button = document.getElementById("reset");
+let p1Display = document.querySelector("#p1Display");
+let p1Score = 0;
+let p2Display = document.getElementById("p2Display");
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
+let resetButton = document.getElementById("reset");
+let numInput = document.querySelector("input");
+let winningScoreDisplay = document.querySelector("p span");
+
+
+p1Button.addEventListener("click", function (){
+	if (!gameOver){
+		p1Score++;
+		console.log(p1Score, winningScore)
+		if (p1Score === winningScore){
+			gameOver = true;
+			p1Display.classList = "winner";
+		}
+	}
+	p1Display.textContent = p1Score; 
+})
+
+p2Button.addEventListener("click", function () {
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === winningScore){
+			gameOver = true;
+			p2Display.classList = "winner";
+		}
+	}
+		p2Display.textContent = p2Score;
+	
+})
+
+function reset () {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+resetButton.addEventListener("click", function () {
+	reset();
+})
+
+
+
+numInput.addEventListener("change", function () {
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var p1Button = document.querySelector("#p1");
+// var p2Button = document.getElementById("p2");
+// var resetButton = document.getElementById("reset");
+// var p1Display = document.querySelector("#p1Display");
+// var p2Display = document.querySelector("#p2Display");
+// var numInput = document.querySelector("input");
+// var winningScoreDisplay = document.querySelector("p span");
+// var p1Score = 0;
+// var p2Score = 0;
+// var gameOver = false;
+// var winningScore = 5;
+
+// p1Button.addEventListener("click", function(){
+// 	if(!gameOver){
+// 		p1Score++;
+// 		if(p1Score === winningScore){
+// 			p1Display.classList.add("winner");
+// 			gameOver = true;
+// 		}
+// 		p1Display.textContent = p1Score;
+// 	}
+// });
+
+// p2Button.addEventListener("click", function(){
+// 	if(!gameOver){
+// 		p2Score++;
+// 		if(p2Score === winningScore){
+// 			p2Display.classList.add("winner");
+// 			gameOver = true;
+// 		}
+// 		p2Display.textContent = p2Score;
+// 	}
+// });
+
+// resetButton.addEventListener("click", function(){
+// 	reset();
+// });
+
+// function reset(){
+// 	p1Score = 0;
+// 	p2Score = 0;
+// 	p1Display.textContent = 0;
+// 	p2Display.textContent = 0;
+// 	p1Display.classList.remove("winner");
+// 	p2Display.classList.remove("winner");
+// 	gameOver = false;
+// }
+
+// numInput.addEventListener("change", function(){
+// 	winningScoreDisplay.textContent = this.value;
+// 	winningScore = Number(this.value);
+// 	reset();
+// });
